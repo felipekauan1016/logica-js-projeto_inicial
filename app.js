b06520628192979b52b6ee66f0549728e411d391
felipekauan1016 <165155524+felipekauan1016@users.noreply.github.com>
2024-03-27 11:07:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+                let numeroSecreto = Math.floor(Math.random()*5);
+                let tentativas = 3;
 
-alert('Bem vindo ao jogo do número secreto');
+                while (Tentativa > 0) {
 
-let numeroSecreto = 3;
-let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
+                    let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
+                    if(numeroDigitado === numeroSecreto){
+                        alert('Parabéns, você acertou!');
+                        break;
+                    } else if(numeroDigitado > numeroSecreto){
+                        alert('Você digitou um número maior que o número secreto');
+                    } else {
+                        alert('Você digitou um número menor que o número secreto');
+                    }
+                    alert (`voce ainda tem ${tentativas} tentativas`);
+                    tentativas = tentativas - 1;
 
-if(numeroDigitado === numeroSecreto){
-    alert('Parabéns, você acertou!');
-} else if(numeroDigitado > numeroSecreto){
-    alert('Você digitou um número maior que o número secreto');
-} else {
-    alert('Você digitou um número menor que o número secreto');
-}
+                }
+
