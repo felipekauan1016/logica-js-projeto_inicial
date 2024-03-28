82a1a830736b961c4a046b2793b5228306f27f52
felipekauan1016 <165155524+felipekauan1016@users.noreply.github.com>
2024-03-28 10:55:12 -0300

    Update soma.js

--- soma.js
+++ soma.js
@@ -1,8 +1,8 @@
 //crie um programa que o usuario entra com dois valores e o sistema soma e imprime em um alert.
 
 // Solicita ao usuário que insira dois valores
-let valor1 = parseInt (prompt ("digite seu primeiro valor"));
-let valor2 = parseInt (prompt ("digite seu segundo valor"));
+let valor1 = parseFloat (prompt ("digite seu primeiro valor"));
+let valor2 = parseFloat (prompt ("digite seu segundo valor"));
 
 // Calculando a soma
 let soma = (valor1 + valor2);
