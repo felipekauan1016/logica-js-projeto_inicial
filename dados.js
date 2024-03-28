ee1ecea0e14a017d84f6a11089ba8cfd2abd3995
felipekauan1016 <165155524+felipekauan1016@users.noreply.github.com>
2024-03-28 10:54:32 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,8 +1,10 @@
 //crie um programa que declare duas variaveis para nome e idade e apareça um alerta com o nome e a idade do usuario
 
+// Solicita ao usuário que insira o nome e a idade
 let nome = prompt ("digite seu nome");
 let idade = prompt ("digite sua idade");
 
+// Cria uma string com as informações do usuário
 alert (`seu nome é: ${nome} e sua idade é: ${idade}`)
 
 
