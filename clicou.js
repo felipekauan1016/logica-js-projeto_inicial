25cac5b4ab6ae824edcd7687f588fbbb904f9005
felipekauan1016 <165155524+felipekauan1016@users.noreply.github.com>
2024-04-04 11:25:35 -0300

    Create clicou.js

---
+++ clicou.js
@@ -0,0 +1,10 @@
+//crie uma funçao que ao clicar ele conta quantas vezes o usuario clicou.
+
+// Inicializa o contador com zero
+let contador = 0; 
+
+// Incrementa o contador a cada clique
+function clicou() {
+  contador++; 
+  alert (`voce clicou em mim ${contador} vezes`)
+}
