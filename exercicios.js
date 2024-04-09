08cfb5dee670515c21683695affe6ac1ea312236
felipekauan1016 <165155524+felipekauan1016@users.noreply.github.com>
2024-04-09 07:56:44 -0300

    Create exercicios.js

---
+++ exercicios.js
@@ -0,0 +1,16 @@
+//1-crie um lista e some seus valores;
+//2-qual o número maior em uma array;
+//3-crie um array e mostre ela no sentido inverso;
+//4-crie uma array e remova os itens duplicados;
+//5-contar o número de vezes que o mesmo elemento aparece na array;
+//6-crie uma função para calcular o fatorial de um número. Use lista;
+
+//1:
+
+function somaArray(arr){
+    let soma;
+    for(let i = 0; i < arr.length; i++){
+    soma += arr(i);
+}
+return soma;
+}
