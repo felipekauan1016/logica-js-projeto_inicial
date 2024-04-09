0771ad5584753fb5dc4bff7d2ff3ffca6dcf6afd
felipekauan1016 <165155524+felipekauan1016@users.noreply.github.com>
2024-04-09 11:27:49 -0300

    Update vogal.js

--- vogal.js
+++ vogal.js
@@ -1,5 +1,4 @@
 // crie uma funçao com um parametro em que o usuario digita uma letra e o sistema retorna um alert se ele é uma vogal ou nao.
-
 function isVogal (letra) {
    
     if (letra === ('a') || letra === ('e') || letra === ('i') || letra === ('o') || letra === ('u')) {
